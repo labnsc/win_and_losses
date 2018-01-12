d92882d563ca8b3d01006b5671685afc379d5faa
Mario Perez <mapreal19@gmail.com>
2018-01-12 12:49:03 +0100

    Handles A B C D

--- lib/line_parser.rb
+++ lib/line_parser.rb
@@ -1,33 +1,55 @@
 class LineParser
   class << self
-    WIN_CODE = '333'.freeze
-    POST_WIN_CODE = '444'.freeze
+    WIN_CODE = "333"
+    POST_WIN_CODE = "444"
 
-    LOSS_CODE = '111'.freeze
-    LOSSES = %w(
+    LOSS_CODE = "111"
+    LOSSES = %w[
       1002
-    ).freeze
+      1004
+      1006
+      1008
+      1009
+      1011
+      1013
+      1014
+      1016
+      1017
+      1020
+      1022
+      1024
+      1025
+      1026
+      1030
+      1032
+      1033
+      1036
+      1037
+      3008
+    ]
 
-    POST_LOSS_CODE = '222'.freeze
-    POST_LOSSES = %w(
+    POST_LOSS_CODE = "222"
+    POST_LOSSES = %w[
       2002
-    ).freeze
+    ]
 
     def call(line)
       code = stimulus_code(line)
 
-      return line.gsub(/S\s*\K(\d+)/, LOSS_CODE) if LOSSES.include?(code)
+      if LOSSES.include?(code)
+        return line.gsub(/S\s*\K(\d+)/, LOSS_CODE)
+      end
 
       if POST_LOSSES.include?(code)
         return line.gsub(/S\s*\K(\d+)/, POST_LOSS_CODE)
       end
 
-      code_number = code.to_i
-      if code_number >= 1000 && code_number < 2000
+      code_first_num = code.chars.first
+      if %w[1 3 5 7].include?(code_first_num)
         return line.gsub(/S\s*\K(\d+)/, WIN_CODE)
       end
 
-      if code_number >= 2000 && code_number < 3000
+      if %w[2 4 6 8].include?(code_first_num)
         return line.gsub(/S\s*\K(\d+)/, POST_WIN_CODE)
       end
     end
@@ -36,4 +58,5 @@ class LineParser
       line.match(/S\s*\K(\d+)/)[0]
     end
   end
+
 end
